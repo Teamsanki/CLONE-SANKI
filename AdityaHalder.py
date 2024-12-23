fb8466bc46b4fa95dad0c22ac4d084fe291b3319
𝐓𝐄𝐀𝐌 𝐒𝐀𝐍𝐊𝐈 <teamsanki001@gmail.com>
2024-12-24 01:44:04 +0530

    Update AdityaHalder.py

--- AdityaHalder.py
+++ AdityaHalder.py
@@ -1313,48 +1313,6 @@ async def check_sping(client, message):
     await m.edit(f"**🤖 Pinged...!!\nLatency:** `{ms}` ms")
 
 
-@bot.on_message(cdx(["repo", "repository"]) & ~pyrofl.bot)
-async def git_repo_link(client, message):
-    if message.sender_chat:
-        mention = message.sender_chat.title
-    else:
-        mention = message.from_user.mention
-    if message.chat.type == ChatType.PRIVATE:
-        caption = f"""**➻ Hello, {mention}
-    
-🥀 I am An ≽ Advanced ≽ High Quality
-Bot, I Can Stream 🌿 Audio & Video In
-Your ♚ Channel And Group.
-
-🐬 Feel Free ≽ To Use Me › And Share
-With Your ☛ Other Friends.**"""
-    else:
-        caption = f"**➻ Hello, {mention}.**"
-    buttons = InlineKeyboardMarkup(
-        [
-            [
-                InlineKeyboardButton(
-                    text="🌺 Open Repository Link 🦋",
-                    url="https://github.com/AdityaHalder/AdityaPlayer",
-                )
-            ],
-            [
-                InlineKeyboardButton(
-                    text="🗑️ Close",
-                    callback_data="force_close",
-                )
-            ],
-        ]
-    )
-    try:
-        await message.reply_photo(
-            photo=START_IMAGE_URL, caption=caption, reply_markup=buttons
-        )
-    except Exception as e:
-        LOGGER.info(f"🚫 Error: {e}")
-        return
-
-
 @bot.on_message(cdx("update") & bot_owner_only)
 async def update_repo_latest(client, message):
     response = await message.reply_text("Checking for available updates...")
